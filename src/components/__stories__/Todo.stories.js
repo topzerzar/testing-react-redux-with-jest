9ff0d4ab227912bff535d4d3281e841ca724e4b6
weerapon <weerapon@thinknet.co.th>
2017-03-20 11:14:35 +0700

    test jest

--- src/components/__stories__/Todo.stories.js
+++ src/components/__stories__/Todo.stories.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import { storiesOf } from '@kadira/storybook';
-import TodoItem from '../TodoItem';
+import React from 'react'
+import { storiesOf } from '@kadira/storybook'
+import TodoItem from '../TodoItem'
+import { withKnobs, text, boolean } from '@kadira/storybook-addon-knobs'
 
 storiesOf('TodoItem', module)
-  .add('Not Have', () => (
-    <TodoItem text="Not Complete" />
+  .addDecorator(withKnobs)
+  .addDecorator(story => (
+    <div style={{ textAlign: 'center' }} >
+      { story() }
+    </div>
+  ))
+  .addWithInfo('not complete', 'An incomplete todo item', () => (
+    <TodoItem text={text('Text', 'Not Complete')} />
   ))
   .add('Have Complete', () => (
-    <TodoItem text="Have Complete" complete />
-  ));
+    <TodoItem text={text('Text', 'Have Complete')} complete={boolean('complete', true)} />
+  ))
---
+++ src/components/__tests__/TodoItem.test.js
@@ -0,0 +1,12 @@
+// Describe what to test
+describe('TodoItem', () => {
+  // Test Scenario
+  it('tests expect 5 tobe 5', () => {
+    expect(5).toBe(5)
+  })
+  // Test Scenario
+  it('check array', () => {
+    expect([1, 2, 3]).toEqual([1, 2, 3])
+  })
+})
+
