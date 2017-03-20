8e741b4e2ac4a3cb8cbd12e31b4f433b62be2768
weerapon <weerapon@thinknet.co.th>
2017-03-20 11:56:54 +0700

    use sanpshot

--- src/components/__tests__/TodoItem.test.js
+++ src/components/__tests__/TodoItem.test.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
 import TodoItem from '../TodoItem'
 
 // Describe what to test
 describe('TodoItem', () => {
   // Test Scenario
+  it('match its snapshot - not complete', () => {
+    const component = shallow(<TodoItem text="Not Complete" />);
+    expect(toJson(component)).toMatchSnapshot()
+  })
+
+  it('match its snapshot - complete', () => {
+    const component = shallow(<TodoItem text="Has Complete" complete />);
+    expect(toJson(component)).toMatchSnapshot()
+  })
+
   it('render correct structure', () => {
     const component = shallow(<TodoItem text="Hello" />);
     expect(component.is('li')).toBe(true)
@@ -13,6 +24,7 @@ describe('TodoItem', () => {
   it('has complete class if receive complete props', () => {
     const component = shallow(<TodoItem text="Hello" complete />);
     expect(component.hasClass('complete')).toBe(true)
+    expect(component.text()).toBe('Hello')
   })
 })
 
